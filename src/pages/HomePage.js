4b82536a25fcb6521735493e1fdc9c2869d0bffa
Dathuong812 <tranthidathuong812@gmail.com>
2023-09-30 15:37:49 +0700

    sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Search from '../modules/home/Search';
 import HomeHeader from '../modules/home/HomeHeader';
+import Content from '../modules/home/Content';
 
 const HomePage = () => {
     return (
         <div className='flex flex-col'>
            <Search></Search>
+           <div className="flex  fle">
            <HomeHeader></HomeHeader>
+           <Content></Content>
+           </div>
         </div>
     );
 };
