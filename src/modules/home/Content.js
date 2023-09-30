fa25b805b4bb2cc38df8a2e09d4c0f6ab8f073dc
Dathuong812 <tranthidathuong812@gmail.com>
2023-09-30 16:39:38 +0700

    lan 3

--- src/modules/home/Content.js
+++ src/modules/home/Content.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Content = () => {
     return (
         <div>
-            <h1 className="pt-5 pl-6 text-2xl font-bold text-text1">CHÚC MỘT NGÀY TỐT LÀNH !</h1>
+            <h1 className="py-5 pl-6 text-2xl font-bold text-text1">CHÚC MỘT NGÀY TỐT LÀNH !</h1>
+            <div className="pt-5 pl-6 mt-3 ml-6 border rounded-lg border-darkStroke bg-slate-400 w-[880px] flex items-center justify-around">
+                <div className="">
+                <h1 className="text-2xl font-bold text-text1 ">
+                    GLUCLOSE xin chào 
+                </h1>
+                <p className="py-4 ">
+                Sức khỏe là thứ mà ta không nhìn thấy được, là yếu tố<br></br> sống còn của mỗi con người. Hãy nâng niu quý trọng <br></br>sức khỏe, đừng để khi mất rồi mới thấy hối tiếc.
+                </p>
+                </div>
+                <img src="./banner.svg" alt="" className="" />
+            </div>
         </div>
     );
 };
