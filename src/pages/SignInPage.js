611f2c120bcc09209ce2735f7a6747d4e7d90aa0
hoadk27 <143632000+hoadk27@users.noreply.github.com>
2023-09-30 18:13:50 +0700

    fix: authen page and layout

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import LayoutAuthentication from '../layout/LayoutAuthentication';
-import FormField from '../components/common/FormField';
+import { useForm } from 'react-hook-form';
 import { Label } from '../components/label';
 import { Input } from '../components/input';
-import { useForm } from 'react-hook-form';
+import FormField from '../components/common/FormField';
 import { Button } from '../components/button';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { CheckBox } from '../components/checkbox';
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import IconEyes from '../components/icons/IconEyes';
 
 const schema = yup.object({
+    name: yup.string().required("This field is required"),
     email: yup
       .string()
       .email("Invalid email address")
@@ -20,7 +22,8 @@ const schema = yup.object({
       .required("This field is required")
       .min(8, "Password must be 8 character "),
   });
-  const SignInPage = () => {
+
+const SignUpPage = () => {
     const {
         handleSubmit,
         control,
@@ -31,38 +34,68 @@ const schema = yup.object({
         mode: "onSubmit",
       });
  
-  const handleSignIn =  (values) => {
+  const handleSignUp =  (values) => {
 
   };
-
+    const [acceptTerm , setAcceptTerm] = useState(false)
     const [showPassword , setShowPassword] = useState(false)
+    const handleToggleTerm =()=>{
+    setAcceptTerm(!acceptTerm)
+   }
    const handleTogglePassword =()=>{
      setShowPassword(!showPassword)
    }
     return (
-        <LayoutAuthentication heading="Welcome back !">
-            <form onSubmit ={handleSubmit(handleSignIn)}>
+        <LayoutAuthentication heading="Đăng kí">
+           <form onSubmit ={handleSubmit(handleSignUp)}>
                 <FormField>
-                <Label>Email*</Label>
-                <Input control ={control} name="email" type="email" placeholder="thuong@gmail.com" error={errors.email?.message}></Input>
+                    <Label>Họ và tên*</Label>
+                    <Input 
+                        control ={control} 
+                        name="name" 
+                        placeholder="Tran Van A"
+                        error={errors.name?.message}
+                        >
+                    </Input>
                 </FormField>
                 <FormField>
-                <Label>Password*</Label>
-                <Input control ={control} name="password" type ={`${showPassword ? "text" : "password"}`} placeholder="create a password"  error={errors.password?.message}>
-                    <IconEyes
+                    <Label>Email*</Label>
+                    <Input 
+                        control ={control} 
+                        name="email" 
+                        type="email" 
+                        placeholder="example: shecodes@gmail.com"
+                        error={errors.email?.message}
+                        >
+                    </Input>
+                </FormField>
+                <FormField>
+                    <Label>Password*</Label>
+                    <Input 
+                        control={control}
+                        name="password"
+                        type ={`${showPassword ? "text" : "password"}`}
+                        placeholder="Tạo mật khẩu"
+                        error={errors.password?.message}
+                        >
+                        <IconEyes
                             open={showPassword}
                             onClick={handleTogglePassword}
-                        ></IconEyes>
-                </Input>
+                            ></IconEyes>
+                    </Input>
                 </FormField>
-                <NavLink to={"/"} className='flex items-end justify-end mb-3 text-primary mr-11 '>Forgot your password ?</NavLink>
-                <Button className='w-[355px] bg-primary' type="submit">
-                    Sign In
+                <div className="flex items-start mb-3 gap-x-2 lg:gap-x-3 lg:mx-11">
+                    <CheckBox name="term" checked={acceptTerm} onClick={handleToggleTerm}>
+                    <p className="text-sm text-[9px] lg:text-base font-medium text-text3 flex-1">Tôi đã đọc và đồng ý <span className='text-primary'>Điều khoản</span> và <span className="text-primary">Chính sách bảo mật</span></p>
+                    </CheckBox>
+                </div>
+                <Button className='w-[355px] bg-primary ' type="submit">
+                    Đăng kí
                 </Button>
-            </form>
-            <p className='mt-3 text-center text-text3 '>Don’t have an account?<Link to="/sign-up" className='text-primary'>Sign Up</Link></p>
+           </form>
+           <p className='mt-3 text-center text-text3 '>Bạn đã có tài khoản?<Link to="/sign-in" className='text-primary'> Đăng nhập</Link></p>
         </LayoutAuthentication>
     );
 };
 
-export default SignInPage;
+export default SignUpPage;
